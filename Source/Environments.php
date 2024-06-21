8f9c6dc0467cb110cf8b642224b8027d7f81e435
Morteza Poussaneh <morteza@protonmail.com>
2024-06-21 09:57:46 +0200

    Try to use github token from environment when there is no token provided in the credential file

--- Source/Environments.php
+++ Source/Environments.php
@@ -8,14 +8,26 @@ use Phpkg\Classes\Environment;
 use PhpRepos\FileManager\JsonFile;
 use const Phpkg\Git\GitHub\GITHUB_DOMAIN;
 
-function has_github_token(Environment $environment): bool
+function has_github_token_env(Environment $environment): bool
+{
+    return $environment->github_token !== null;
+}
+
+function has_defined_github_credential(Environment $environment): bool
 {
     return Credentials::from_array(JsonFile\to_array($environment->credential_file))
         ->has(fn (Credential $credential) => $credential->key === GITHUB_DOMAIN && strlen($credential->value) > 0);
 }
 
+function has_github_token(Environment $environment): bool
+{
+    return has_defined_github_credential($environment) || has_github_token_env($environment);
+}
+
 function github_token(Environment $environment): string
 {
-    return Credentials::from_array(JsonFile\to_array($environment->credential_file))
-        ->first(fn (Credential $credential) => $credential->key === GITHUB_DOMAIN)->value;
+    return has_defined_github_credential($environment)
+        ? Credentials::from_array(JsonFile\to_array($environment->credential_file))
+            ->first(fn (Credential $credential) => $credential->key === GITHUB_DOMAIN)->value
+        : $environment->github_token;
 }
--- Source/System.php
+++ Source/System.php
@@ -17,9 +17,12 @@ function random_temp_directory(): string
 
 function environment(): Environment
 {
+    $github_token_var = getenv('GITHUB_TOKEN');
+
     return new Environment(
         Path::from_string($_SERVER['PWD']),
         Path::from_string($_SERVER['SCRIPT_FILENAME'])->sibling('credentials.json'),
         Path::from_string(sys_get_temp_dir())->append('phpkg'),
+        strlen($github_token_var) > 0 ? $github_token_var : null,
     );
 }
