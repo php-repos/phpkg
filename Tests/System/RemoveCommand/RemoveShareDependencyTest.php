ff081ebafec366c10dd70aca19ad9ad28b3959b8
Morteza Poussaneh <morteza@protonmail.com>
2024-07-22 08:17:55 +0200

    Test

--- Tests/System/RemoveCommand/RemoveShareDependencyTest.php
+++ Tests/System/RemoveCommand/RemoveShareDependencyTest.php
@@ -2,12 +2,39 @@
 
 namespace Tests\System\RemoveCommand\RemoveShareDependencyTest;
 
+use Phpkg\Classes\Project;
 use PhpRepos\FileManager\JsonFile;
+use PhpRepos\FileManager\Path;
+use function PhpRepos\Cli\Output\assert_success;
+use function PhpRepos\FileManager\Directory\exists;
 use function PhpRepos\FileManager\Resolver\root;
 use function PhpRepos\FileManager\Resolver\realpath;
 use function PhpRepos\TestRunner\Assertions\Boolean\assert_true;
 use function PhpRepos\TestRunner\Runner\test;
 use function Tests\Helper\reset_empty_project;
+
+test(
+    title: 'it should remove package and its subpackages',
+    case: function () {
+        $output = shell_exec('php ' . root() . 'phpkg remove git@github.com:php-repos/test-runner.git --project=TestRequirements/Fixtures/EmptyProject');
+
+        $lines = explode("\n", trim($output));
+        assert_success("Package git@github.com:php-repos/test-runner.git has been removed successfully.", $lines[4] . PHP_EOL);
+
+        $project = Project::initialized(Path::from_string(root() . '/TestRequirements/Fixtures/EmptyProject'));
+
+        assert_true($project->config->packages->count() === 0, 'config packages has not been cleaned up properly!');
+        assert_true($project->meta->packages->count() === 0, 'meta config packages has not been cleaned up properly!');
+        assert_true(! exists($project->packages_directory->append('php-repos/test-runner')), 'Packages files are not deleted!');
+    },
+    before: function () {
+        shell_exec('php ' . root() . 'phpkg init --project=TestRequirements/Fixtures/EmptyProject');
+        shell_exec('php ' . root() . 'phpkg add git@github.com:php-repos/test-runner.git --version=v1.1.0 --project=TestRequirements/Fixtures/EmptyProject');
+    },
+    after: function () {
+        reset_empty_project();
+    }
+);
 
 test(
     title: 'it should remove the package but leave used subpackage',
